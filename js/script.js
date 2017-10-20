90eb04bfcd3a1eefbc8855e07369bea5f7901120
Eivan <enispero@sfu.ca>
2017-10-20 03:18:18 -0700

    Form Complete
    Form complete, aligned properly with mobile version, styling is also complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   var mobileLinks = document.getElementById('main-nav-bar-hidden');
   var mobileIcon = document.getElementById('nav-toggle');
+  var mobileIconPadding = document.getElementById('nav-toggle-area')
   var mobileLinksA = document.getElementsByClassName('hidden-link')
 
   function mobileMenuCloseOpen(e) {
@@ -8,7 +9,7 @@ window.onload = function() {
       mobileLinks.style.display = 'none';
     }
 
-    if (e.target == mobileIcon) {
+    if (e.target == mobileIcon || e.target == mobileIconPadding) {
       mobileLinks.style.display = 'flex';
     }
   }
